ddd6c75039b4c92b445dc644c4bcfa01a1ccaae4
Pedro Brando <62448483+Pedro-Brando@users.noreply.github.com>
2020-11-19 11:56:17 -0300

    Pause Game script
    Novo script.

---
+++ Assets/Scripts/Pause_Game.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Pause_Game : MonoBehaviour
+{
+    public GameObject pauseMenu;
+    private bool isPaused;
+    public AudioListener audio;
+    void Update()
+    {
+        //Código para pause e unpause do game
+
+        if (Input.GetKeyDown (KeyCode.Escape) && isPaused == false)    {
+             Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+             pauseMenu.gameObject.SetActive (true);
+             AudioListener.pause = true;
+             isPaused = true;
+        }
+        else if (Input.GetKeyDown (KeyCode.Escape) && isPaused == true){
+            Time.timeScale = Mathf.Approximately(Time.timeScale, 0.0f) ? 1.0f : 0.0f;
+            pauseMenu.gameObject.SetActive (false);
+            AudioListener.pause = false;
+            isPaused = false;
+        }
+        
+    }
+}
